590cfe0cacc3e8030f2059c9f4084e33490371e0
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-04 12:26:15 -0300

    Utilização da lib unittest

--- part I/superlists/functional_tests.py
+++ part I/superlists/functional_tests.py
@@ -1,40 +1,44 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-# Edith ouvi falar de uma nova aplicação online interessante para
-# lista de tarefas. Ela decide verificar sua homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# ELa percebe que o título da página e o cabeçalho mencionam listas de tarefas
-assert 'to-do' in browser.title
+    def tearDown(self):        
+        self.browser.quit()
 
-# Ela é convidada a inserir um item de tarefa imediatamente
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouvi falar de uma nova aplicação online interessante para
+        # lista de tarefas. Ela decide verificar sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # ELa percebe que o título da página e o cabeçalho mencionam listas de tarefas
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Ela é convidada a inserir um item de tarefa imediatamente
+
+        # ELa digiata "Comprar penas de pavão" em uma caixa
+        # de texto 
+
+        # Quando ela tecla enter, a página é atualizada, e agora a página lista
+        # "1: Comprar penas de pavão" como um item em uma lista de tarefas
+
+        # Ainda contiua havendo uma caixa de texto convidando-a a acrescentar outro item.
+        # Ela insere "Usar penas de pavão para fazer um fly"
+
+        # A página é atualizada novamente e agora mostra os dois itens em sua lista
+
+        # Edith se pergunta se o site lembrará de sua lista. Então ela nota
+        # que o site gerou um URL único para ela -- há um pequeno
+        # text explicativo para isso.
+
+        # Ela acessa essa URL -  sua lista de tarefas continua lá.
+
+        # Satisfeita ela volta a dormir
 
 
-# ELa digiata "Comprar penas de pavão" em uma caixa
-# de texto 
-
-
-# Quando ela tecla enter, a página é atualizada, e agora a página lista
-# "1: Comprar penas de pavão" como um item em uma lista de tarefas
-
-
-# Ainda contiua havendo uma caixa de texto convidando-a a acrescentar outro item.
-# Ela insere "Usar penas de pavão para fazer um fly"
-
-
-# A página é atualizada novamente e agora mostra os dois itens em sua lista
-
-
-# Edith se pergunta se o site lembrará de sua lista. Então ela nota
-# que o site gerou um URL único para ela -- há um pequeno
-# text explicativo para isso.
-
-
-# Ela acessa essa URL -  sua lista de tarefas continua lá.
-
-
-# Satisfeita ela volta a dormir
-
-browser.quit()
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
