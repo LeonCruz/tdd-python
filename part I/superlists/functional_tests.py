ee3be548740e69363e504adf7188a288b7d91f37
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-04 15:50:59 -0300

    Continução dos testes funcionais

--- part I/superlists/functional_tests.py
+++ part I/superlists/functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouvi falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
@@ -37,24 +42,24 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Comprar penas de pavão" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Comprar penas de pavão' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.check_for_row_in_list_table('1: Comprar penas de pavão')
 
         # Ainda contiua havendo uma caixa de texto convidando-a a acrescentar
         # outro item. Ela insere "Usar penas de pavão para fazer um fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Usar penas de pavão para fazer um fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua
         # lista
+        self.check_for_row_in_list_table('1: Comprar penas de pavão')
+        self.check_for_row_in_list_table('2: Usar penas de pavão para fazer um fly')
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota
         # que o site gerou um URL único para ela -- há um pequeno
         # text explicativo para isso.
+        self.fail('Finish the test!')
 
         # Ela acessa essa URL -  sua lista de tarefas continua lá.
 
