50c7aa8cf4d0431d0eece4cd7e81648795cd81cf
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-06-04 21:08:45 -0300

    Remoçao do sleeps

--- part I/superlists/functional_tests/tests.py
+++ part I/superlists/functional_tests/tests.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 import time
 import unittest
+
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,10 +16,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouvi falar de uma nova aplicação online interessante para
@@ -42,20 +56,18 @@ class NewVisitorTest(LiveServerTestCase):
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Comprar penas de pavão" como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Comprar penas de pavão')
+        self.wait_for_row_in_list_table('1: Comprar penas de pavão')
 
         # Ainda contiua havendo uma caixa de texto convidando-a a acrescentar
         # outro item. Ela insere "Usar penas de pavão para fazer um fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Usar penas de pavão para fazer um fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua
         # lista
-        self.check_for_row_in_list_table('1: Comprar penas de pavão')
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table('1: Comprar penas de pavão')
+        self.wait_for_row_in_list_table(
             '2: Usar penas de pavão para fazer um fly'
         )
 
